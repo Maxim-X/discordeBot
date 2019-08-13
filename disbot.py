67c09a6a647b26b3467e9047b6c76adcfbefdca6
Maxim-X <43073464+Maxim-X@users.noreply.github.com>
2019-08-13 16:45:04 -0300

    Update disbot.py

--- disbot.py
+++ disbot.py
@@ -7,4 +7,4 @@ bot = commands.Bot(command_prefix='>')
 async def ping(ctx):
     await ctx.send('pong')
 
-bot.run('NTcxMzUzNDI3NzI1MjU0NjU3.XVMNvQ.K1jIE0dq7mhc5zQfHWi8_qdaHqk')
+bot.run('NTcxMzUzNDI3NzI1MjU0NjU3.XVMTKA.xNV2EIXdHNING4UKmajH48Lp4Zg')
