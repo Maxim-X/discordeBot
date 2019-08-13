4eba13ffbbe806705720922ceac33bf0460efcb4
Maxim-X <43073464+Maxim-X@users.noreply.github.com>
2019-08-13 16:43:06 -0300

    Update disbot.py

--- disbot.py
+++ disbot.py
@@ -1,16 +1,10 @@
 import discord
 from discord.ext import commands
-import os
 
-Bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='>')
 
-@Bot.event
-async def on_ready():
-	print('Бот онлайн!')
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
-@Bot.command(pass_context = True)
-async def hello(ctx):
-	await Bot.say("Hello!!!")
-
-token = os.environ.get('BOT_TIKEN')
-Bot.run(str(token))
+bot.run('NTcxMzUzNDI3NzI1MjU0NjU3.XVMNvQ.K1jIE0dq7mhc5zQfHWi8_qdaHqk')
