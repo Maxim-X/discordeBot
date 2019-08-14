3bee70e138c8f83a08410eeecb4a67ce26397334
Maxim-X <maksposter007@gmail.com>
2019-08-14 17:46:13 -0300

    Update disbot.py

--- disbot.py
+++ disbot.py
@@ -22,7 +22,7 @@ async def myinfo(ctx, user: discord.User):
 	embd.add_field(name = "Name", value= "Value")
 	embd.set_thumbnail(url= user.avatar_url)
 	embd.set_author(name= "Author", url= user.avatar_url)
-    await self.bot.say(embed = embd)
+    await bot.say(embed = embd)
 
     
 
