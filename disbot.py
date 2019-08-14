48e91912731cd192dfa293e5bfcc053b8d499726
Maxim-X <43073464+Maxim-X@users.noreply.github.com>
2019-08-14 06:14:43 -0300

    Update disbot.py

--- disbot.py
+++ disbot.py
@@ -7,7 +7,7 @@ bot = commands.Bot(command_prefix='>')
 summ = 1
 
 @bot.command()
-async def ping(ctx, message):
+async def ping(ctx):
     global summ
     summ+=1
     await ctx.send('pong'+str(summ))
