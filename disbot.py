d0b116a2db5d97b29be16785b27d3c18b1f83049
Maxim-X <43073464+Maxim-X@users.noreply.github.com>
2019-08-14 06:27:15 -0300

    Update disbot.py

--- disbot.py
+++ disbot.py
@@ -4,21 +4,22 @@ import os
 import logging
 
 bot = commands.Bot(command_prefix='>')
-
-summ = 1
-
-@bot.command()
-async def ping(ctx1):
-    global summ
-    summ+=1
-    await ctx.send('pong'+str(summ))
-
-    
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
+
+summ = 1
+
+@bot.command()
+async def ping(ctx):
+    global summ
+    summ+=1
+    await ctx.send('pong'+str(summ))
+
+    
+
     
 token = os.environ.get('BOT_TOKEN')
 
