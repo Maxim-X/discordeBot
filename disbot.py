8db3ed2ed46a84fa456ea39ac51b3a6556c43585
Maxim-X <maksposter007@gmail.com>
2019-08-14 20:56:13 -0300

    Update disbot.py

--- disbot.py
+++ disbot.py
@@ -12,11 +12,10 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-summ = 1
 
 @bot.command(pass_context= True)
 async def hello(ctx):
-	await ctx.send("Hello1 {}".format(ctx.message.author.mention))
+	await ctx.send("Привет, {}".format(ctx.message.author.mention))
 
 @bot.command(pass_context= True)
 async def myinfo(ctx, user: discord.User):
