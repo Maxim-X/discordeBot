cb31cb4ecb9c9a3cdf4db8351a47efef3b198960
Maxim-X <43073464+Maxim-X@users.noreply.github.com>
2019-08-14 07:42:39 -0300

    Update disbot.py

--- disbot.py
+++ disbot.py
@@ -2,9 +2,8 @@ import discord
 from discord.ext import commands
 import os
 import logging
-import discord.ext.commands import Bot
 
-Bot = commands.Bot(command_prefix='>')
+bot = commands.Bot(command_prefix='>')
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
@@ -13,22 +12,21 @@ logger.addHandler(handler)
 
 summ = 1
 
-@Bot.command(pass_context= True)
+@bot.command()
 async def ping(ctx):
     global summ
     summ+=1
     global user
     user = ctx.message.author
-    await Bot.say('pong'+str(user))
+    await ctx.send('pong'+str(user))
 
-@Bot.command(pass_context= True)
+@bot.command(pass_context= True)
 async def myinfo(ctx, user: discord.User):
-    await Bot.say("Name: {}".format(user.name))
+	await ctx.send("Name: {}".format(user.name))
 
     
 
     
 token = os.environ.get('BOT_TOKEN')
 
-Bot.run(str(token))
-
+bot.run(str(token))
