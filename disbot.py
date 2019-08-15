d72c924e9c01f0d74423b8b55564319371bfd5d2
Maxim-X <maksposter007@gmail.com>
2019-08-14 22:52:35 -0300

    Update disbot.py

--- disbot.py
+++ disbot.py
@@ -12,7 +12,10 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-
+@bot.event
+async def on_message(msg):
+	if msg.content == "qwe":
+		await bot.delete_message(msg)
 
 @bot.command(pass_context= True)
 async def hello(ctx):
