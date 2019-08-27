11cda55eb58d2ed72a36a6144c95756a956f0ab5
Maxim-X <maksposter007@gmail.com>
2019-08-27 15:54:59 -0300

    Update disbot.py

--- disbot.py
+++ disbot.py
@@ -50,7 +50,7 @@ async def time(ctx):
 @bot.command(pass_context= True)
 async def pars(ctx):
 	await bot.wait_until_ready()
-	channel = bot.get_channel(412939700748419086)
+	channel = bot.get_channel(615296305144660008)#412939700748419086
 	driver.get('https://www.epicgames.com/store/ru/')
 	# assert 'Yahoo' in browser.title
 
@@ -67,6 +67,7 @@ async def pars(ctx):
 		allImgGame = login_form[0].find_elements_by_xpath("//*[starts-with(@class, 'FreeGame-inner')]")
 		ImgGame = allImgGame[0].get_attribute("src")
 		allTime = login_form[0].find_elements_by_xpath("//time")
+		allUrlGame = login_form[0].find_elements_by_xpath("//a[starts-with(@class, 'FreeGame-gameCard')]").get_attribute('href')
 		print(nameGameOk)
 		print(str(ImgGame))
 		print(str(allTime[0].text))
@@ -83,11 +84,14 @@ async def pars(ctx):
 		nameGame = driver.find_elements_by_xpath("//*[starts-with(@class, 'StoreCard-title')]")
 		nameGameOk ="``"+nameGame[0].text +"`` , ``"+ nameGame[1].text+"``"
 		embed=discord.Embed(title="Бесплатные игры в Epic Games | Store", description="Привет всем участникам канала!\nСейчас в магазине Epic Games | Store бесплатно раздается: "+str(nameGameOk)+"\n\nДанные игры будут бесплатны до "+str(timeGame.replace('.',''))+", успей добавить их в свою библиотеку!", color=0x0078f2)
+		allUrlGame = 'https://www.epicgames.com/store/ru/collection/free-games-collection'
+
 
 	driver.quit()
 	embed.set_image(url=""+str(ImgGame)+"")
 	embed.set_footer(text="Сервер "+str(bot.guilds[0].name))
 	await channel.send(embed=embed)
+	await channel.send(str(allUrlGame))
 
 # Now you can start using Selenium
 
