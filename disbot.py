56734b04342f8bf4ff7a80c4e2495d054cde3c12
Maxim-X <maksposter007@gmail.com>
2019-08-27 17:58:53 -0300

    Update disbot.py

--- disbot.py
+++ disbot.py
@@ -39,6 +39,14 @@ async def time(ctx):
 
 @bot.command(pass_context= True)
 async def news(ctx):
+	#--- Парсинг сайтов
+	chrome_options = webdriver.ChromeOptions()
+	chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+	chrome_options.add_argument("--headless")
+	chrome_options.add_argument("--disable-dev-shm-usage")
+	chrome_options.add_argument("--no-sandbox")
+	driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+	#--- Парсинг сайтов
 	driver.get('https://www.playground.ru/news/pc/')
 	pageListUrl = driver.find_element_by_xpath('//a[@class="item story-container"]')
 	pageGame = pageListUrl.get_attribute('href')
