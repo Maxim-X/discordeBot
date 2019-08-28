9abd849dbd36919d86c0f443b43d63cdba423b47
Maxim-X <maksposter007@gmail.com>
2019-08-28 17:37:14 -0300

    1

--- disbot.py
+++ disbot.py
@@ -15,7 +15,6 @@ import asyncio
 from selenium import webdriver
 import random
 from discord.utils import get
-import youtube_dl
 bot = commands.Bot(command_prefix='>')
 # logger = logging.getLogger('discord')
 # logger.setLevel(logging.DEBUG)
@@ -566,92 +565,6 @@ async def cleanChat(ctx, allNumMessage):
 			await ctx.send('Пользователь со статусом `Яндекс.Уборщик` может удалить до 10 сообщений за раз.\nЕсли вы хотите очистить чат полностью обратитесь к `Maxim`')
 	else:
 		await ctx.send("Пользователь без статуса `Яндекс.Уборщик` не может массово удалять сообщения.")
-
-
-
-@bot.command(pass_context=True, aliases=['j', 'joi'])
-async def join(ctx):
-    discord.opus.load_opus()
-    if not discord.opus.is_loaded():
-        raise RunTimeError('Opus failed to load')
-    channel = ctx.message.author.voice.channel
-    voice = get(bot.voice_clients, guild=ctx.guild)
-
-    if voice and voice.is_connected():
-        await voice.move_to(channel)
-    else:
-        voice = await channel.connect()
-
-    await voice.disconnect()
-
-    if voice and voice.is_connected():
-        await voice.move_to(channel)
-    else:
-        voice = await channel.connect()
-        print(f"The bot has connected to {channel}\n")
-
-    await ctx.send(f"Joined {channel}")
-
-
-@bot.command(pass_context=True, aliases=['l', 'lea'])
-async def leave(ctx):
-    channel = ctx.message.author.voice.channel
-    voice = get(bot.voice_clients, guild=ctx.guild)
-
-    if voice and voice.is_connected():
-        await voice.disconnect()
-        print(f"The bot has left {channel}")
-        await ctx.send(f"Left {channel}")
-    else:
-        print("Bot was told to leave voice channel, but was not in one")
-        await ctx.send("Don't think I am in a voice channel")
-
-
-
-
-@bot.command(pass_context=True, aliases=['p', 'pla'])
-async def play(ctx, url: str):
-
-    song_there = os.path.isfile("song.mp3")
-    try:
-        if song_there:
-            os.remove("song.mp3")
-            print("Removed old song file")
-    except PermissionError:
-        print("Trying to delete song file, but it's being played")
-        await ctx.send("ERROR: Music playing")
-        return
-
-    await ctx.send("Getting everything ready now")
-
-    voice = get(bot.voice_clients, guild=ctx.guild)
-
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-    }
-
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        print("Downloading audio now\n")
-        ydl.download([url])
-
-    for file in os.listdir("./"):
-        if file.endswith(".mp3"):
-            name = file
-            print(f"Renamed File: {file}\n")
-            os.rename(file, "song.mp3")
-
-    voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: print("Song done!"))
-    voice.source = discord.PCMVolumeTransformer(voice.source)
-    voice.source.volume = 0.07
-
-    nname = name.rsplit("-", 2)
-    await ctx.send(f"Playing: {nname[0]}")
-    print("playing\n")
 
 
 
