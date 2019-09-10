ef11921d5287fb3f983a6f911420f3bc751c07e9
Maxim-X <maksposter007@gmail.com>
2019-09-10 18:36:31 -0300

    Update disbot.py

--- disbot.py
+++ disbot.py
@@ -499,6 +499,14 @@ async def hello(ctx):
 
 @bot.command(pass_context= True)
 async def mem(ctx):
+	#--- Парсинг сайтов
+	chrome_options = webdriver.ChromeOptions()
+	chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+	chrome_options.add_argument("--headless")
+	chrome_options.add_argument("--disable-dev-shm-usage")
+	chrome_options.add_argument("--no-sandbox")
+	driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+	#--- Парсинг сайтов
 	driver.get('https://admem.ru/rndm')
 	imgMem = driver.find_element_by_xpath('//div[@class="post"]//img')
 	imgMemSrc = imgMem.get_attribute('src')
