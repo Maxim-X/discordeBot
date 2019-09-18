ee45b1158beef8455bd3a2fb493346b7a75663b9
Maxim-X <maksposter007@gmail.com>
2019-09-18 18:04:01 -0300

    Update disbot.py

--- disbot.py
+++ disbot.py
@@ -26,7 +26,7 @@ def chromeOpen():
 	chrome_options.add_argument("--headless")
 	chrome_options.add_argument("--disable-dev-shm-usage")
 	chrome_options.add_argument("--no-sandbox")
-	driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+	return driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 	#--- Парсинг сайтов
 
 @bot.command(pass_context= True)
