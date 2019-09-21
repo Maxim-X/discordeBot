a8ed889601350e5b199be5a01e902ac917b915b4
Maxim-X <maksposter007@gmail.com>
2019-09-20 21:42:54 -0300

    Update disbot.py

--- disbot.py
+++ disbot.py
@@ -15,7 +15,6 @@ import asyncio
 from selenium import webdriver
 import random
 from discord.utils import get
-import json
 bot = commands.Bot(command_prefix='>')
 
 
@@ -37,29 +36,7 @@ async def time(ctx):
 
 
 
-# @bot.command(pass_context= True)
-# async def music(ctx):
-# 	channel = ctx.message.author.voice.channel
-# 	voice = get(bot.voice_clients, guild=ctx.guild)
-# 	if voice and voice.is_connected():
-# 		await voice.move_to(channel)
-# 	else:
-# 		voice = await channel.connect()
-# 	await ctx.process_commands(">mem")
 
-# @bot.command(pass_context=True)
-# async def add(ctx):
-	
-
-# 	data = {
-# 		"president": {
-# 			"name": "Zaphod Beeblebrox",
-# 			"species": "Betelgeusian",
-# 			"user": "Maxim"
-# 		}
-# 	}
-# 	with open('listBunMessage.json','w') as f:
-# 		json.dump(data, f)
 
 async def newsGamePlayGround():
 	while not bot.is_closed():
