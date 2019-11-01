16cc5e9b3bbb0730c2aca1e6227ef5f3d5d71d88
Maxim-X <maksposter007@gmail.com>
2019-11-01 15:22:11 +0900

    Update disbot.py

--- disbot.py
+++ disbot.py
@@ -109,7 +109,7 @@ async def pars(ctx):
 	await asyncio.sleep(5)
 	login_formAllSection = driver.find_elements_by_xpath("//*[starts-with(@class, 'Discover-section_')]")
 
-	print("\t ----- "+ login_formAllSection[2].html+"\n\n")
+	print("\t ----- "+ login_formAllSection[2].text+"\n\n")
 	login_form = login_formAllSection[2].find_elements_by_xpath("//*[starts-with(@class, 'CardGrid-card')]")
 	nameGame = login_form[0].find_elements_by_xpath("//*[starts-with(@class, 'Card-title_')]")
 	nameGameOk = nameGame[0].text
