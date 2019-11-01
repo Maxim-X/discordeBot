60882414c689b2ef46d60042e68808223b6097c6
Maxim-X <maksposter007@gmail.com>
2019-11-01 16:29:58 +0900

    Update disbot.py

--- disbot.py
+++ disbot.py
@@ -112,7 +112,7 @@ async def pars(ctx):
 	print("\t 1----- "+ str(login_formAllSection[2].text)+"\n\n")
 	login_form = login_formAllSection[2];
 	print("\t 2----- "+str(login_form.text)+"\n\n")
-	login_form = login_form.find_elements_by_xpath("./*[starts-with(@class, 'CardGrid-card')]")
+	login_form = login_form.find_elements_by_xpath("/*[starts-with(@class, 'CardGrid-card')]")
 	#print("\t 3----- "+str(login_form.text)+"\n\n")
 	#nameGame = login_form[0].find_elements_by_xpath("//*[starts-with(@class, 'Card-title_')]")
 	#nameGameOk = nameGame[0].text
