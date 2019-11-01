d5448a02abf90359a6a779384268d0a7c8d11f75
Maxim-X <maksposter007@gmail.com>
2019-11-01 14:38:13 +0900

    Update disbot.py

--- disbot.py
+++ disbot.py
@@ -107,7 +107,7 @@ async def pars(ctx):
 	# elem.send_keys('seleniumhq' + Keys.RETURN)
 	# time.sleep(5)
 	await asyncio.sleep(5)
-	login_form = driver.find_elements_by_xpath("//*[starts-with(@class, 'CardGrid-group')]")
+	login_form = driver.find_elements_by_xpath("//*[starts-with(@class, 'CardGrid-groupWrapper')]")
 	nameGame = login_form[1].find_elements_by_xpath("//*[starts-with(@class, 'Card-title_')]")
 	nameGameOk = nameGame[0].text
 	if nameGameOk != 'Бесплатные игры':
