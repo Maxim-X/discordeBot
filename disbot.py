4a948abb68b6b292921dbdec3150612241506abc
Maxim-X <maksposter007@gmail.com>
2019-11-09 18:18:08 +0900

    Update disbot.py

--- disbot.py
+++ disbot.py
@@ -146,7 +146,7 @@ async def pars(ctx):
 		# 	UrlGame = 'https://www.epicgames.com/store/ru/collection/free-game-collection'
 		await asyncio.sleep(5)
 		# login_form = driver.find_elements_by_xpath("//*[starts-with(@class, 'FreeGame-game')]")
-		nameGame = driver.find_elements_by_xpath("//*[starts-with(@class, 'Discover-contentWrapper_')]/span[3]//*[starts-with(@class, 'StoreCard-title')]")
+		nameGame = driver.find_elements_by_xpath("//*[starts-with(@class, 'StoreCard-title')]")
 		nameGameOk ="``"+nameGame[0].text +"`` , ``"+ nameGame[1].text+"``"
 		embed=discord.Embed(title="Бесплатные игры в Epic Games | Store", description=f"Привет всем участникам канала!\nСейчас в магазине Epic Games | Store бесплатно раздается: {nameGameOk}\n\nДанные игры будут бесплатны до {timeGameOk}, успей добавить их в свою библиотеку!\n[Ссылка на игры]({UrlGame})", color=0xff7d25)
 		
